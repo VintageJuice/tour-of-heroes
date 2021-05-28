28f5b825a8bb10345749ec9203c4a465200aadd2
patrick <pfoy23@gmail.com>
2021-05-28 20:38:31 +0200

    Added a list of heroes to displayed, learned about: - importing into components. - ngFor for looping data in elements. - ngIf for rendering to the dom conditionally. - ngModel two way binding. - adding an event handler via (click). - styling with CSS through the @Component definition.

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -5,20 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { FormsModule } from '@angular/forms';
-import { UpperCasePipe } from '@angular/common';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent,
-    UpperCasePipe
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule
   ],
-  providers: [UpperCasePipe],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
